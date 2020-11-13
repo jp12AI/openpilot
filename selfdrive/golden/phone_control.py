ab563771ab14337134dcd095cc1d137bea6bf352
Golden Li <lixin2@csvw.com>
2020-11-13 11:02:18 +0800

    update

--- selfdrive/golden/phone_control.py
+++ selfdrive/golden/phone_control.py
@@ -226,6 +226,7 @@ def main():
 
     # if ip is not connected, try to reconnect
     if not ip:
+      time.sleep(1)
       ip = try_to_connect(last_ip)
       if ip:
         sync_sock = create_sub_sock(ip, sync_content, timeout=TIME_OUT)
