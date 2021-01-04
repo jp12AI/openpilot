1dd3ef643d45018f349845aeec5dc593a2da09b6
Golden Li <lixin2@csvw.com>
2021-01-04 10:06:04 +0800

    update

--- selfdrive/golden/kill_all.sh
+++ selfdrive/golden/kill_all.sh
@@ -4,7 +4,7 @@ ps -ef | grep 'thermald' | grep -v grep | awk '{print $1}' | xargs -r kill -9
 ps -ef | grep 'manager.py' | grep -v grep | awk '{print $1}' | xargs -r kill -9
 ps -ef | grep 'phone_control' | grep -v grep | awk '{print $1}' | xargs -r kill -9
 
-ps -ef | grep 'selfdrive' | grep -v grep | awk '{print $1}' | xargs -r kill -9
+ps -ef | grep 'manage_athenad' | grep -v grep | awk '{print $1}' | xargs -r kill -9
 ps -ef | grep 'launch_chffrplus' | grep -v grep | awk '{print $1}' | xargs -r kill -9
 ps -ef | grep 'start_op' | grep -v grep | awk '{print $1}' | xargs -r kill -9
 
