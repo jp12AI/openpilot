e3b5cc00f3cc145e6014c03bfdc87d34e2eee848
Golden Li <lixin2@csvw.com>
2021-01-08 09:07:29 +0800

    refactor can simulation script maybe a simple simulator

--- selfdrive/golden/can_bridge.py
+++ selfdrive/golden/can_bridge.py
@@ -11,77 +11,61 @@ import argparse
 from common.params import Params
 from common.realtime import Ratekeeper
 from selfdrive.golden.can import can_function, sendcan_function
+from selfdrive.car.honda.values import CruiseButtons
 import queue
 import subprocess
 
 import sys
 import signal
 
+params = Params()
+
 def main():
+
+  global params
+  params.delete("Offroad_ConnectivityNeeded")
+  params.delete("CalibrationParams")
+  params.put("CalibrationParams", '{"calib_radians": [0,0,0], "valid_blocks": 20}')
+
   os.system('echo 1 > /tmp/op_simulation')
   os.system('echo 1 > /tmp/force_calibration')
   os.system('service call audio 3 i32 3 i32 0 i32 1')
 
+  rom multiprocessing import Process, Queue
+  q = Queue()
+  from selfdrive.golden.keyboard_ctrl import keyboard_poll_thread
+    keyboard_poll_thread(q)
+
   global pm
 
   pm = messaging.PubMaster(['can', 'health'])
-  gps = messaging.sub_sock('gpsLocation')
-  live_params = messaging.sub_sock('liveParameters')
-  #live_calibartion = messaging.sub_sock('liveCalibration')
 
   # can loop
   sendcan = messaging.sub_sock('sendcan')
   rk = Ratekeeper(100, print_delay_threshold=None)
   steer_angle = 0.0
-
-  gps_speed = 50.0 / 3.6
-  cal_status = 0
-  posenet_speed = 0.0
-  btn_list = []
+  speed = 50.0 / 3.6
+  engage = False
+  cruise_button = 0
 
   while 1:
-    gps_data = messaging.recv_sock(gps)
-    params = messaging.recv_sock(live_params)
-    calibration = None
-    #calibration = messaging.recv_sock(live_calibartion)
 
-    if gps_data:
-      gps_speed = gps_data.gpsLocation.speed
+    # check keyboard input
+    if not q.empty():
+      message = q.get()
+      m = message.split('_')
+      if m[0] == "cruise":
+        if m[1] == "down":
+          cruise_button = CruiseButtons.DECEL_SET
+          engage = True
+        if m[1] == "up":
+          cruise_button = CruiseButtons.RES_ACCEL
+          engage = True
+        if m[1] == "cancel":
+          cruise_button = CruiseButtons.CANCEL
+          engage = False
 
-    if params:
-      posenet_speed = params.liveParameters.posenetSpeed
-      #print ('posenet_speed=' + str(posenet_speed*3.6) + ' kph')
-
-    if calibration:
-      cal_status = calibration.liveCalibration.calStatus
-
-    engage = False
-    if rk.frame != 0 and rk.frame % 500 == 0:
-      engage = cal_status == 1
-
-    speed = gps_speed
-    if speed < 0.00001:
-      speed = posenet_speed
-    #can_function(pm, speed, steer_angle, rk.frame, rk.frame%500 == 499)
-
-    if os.path.exists('/tmp/op_start'):
-      if len(btn_list) == 0:
-        for x in range(10):
-          btn_list.append(3)
-        os.system('rm /tmp/op_start')
-
-    if os.path.exists('/tmp/op_stop'):
-      if len(btn_list) == 0:
-        for x in range(10):
-          btn_list.append(2)
-      os.system('rm /tmp/op_stop')
-
-    btn = 0
-    if len(btn_list) > 0:
-      btn = btn_list[0]
-      btn_list.pop(0)
-
-    can_function(pm, speed * 3.6, steer_angle, rk.frame, cruise_button=btn, is_engaged=1)
+    can_function(pm, speed * 3.6, steer_angle, rk.frame, cruise_button=cruise_button, is_engaged=engage)
     #if rk.frame%5 == 0:
     #  throttle, brake, steer = sendcan_function(sendcan)
     #  steer_angle += steer/10000.0 # torque
@@ -117,10 +101,19 @@ def signal_handler(sig, frame):
       pm.send('health', dat)
       time.sleep(0.1)
 
+    global params
+    params.delete("CalibrationParams")
+
     print ("exiting")
     sys.exit(0)
 
 if __name__ == "__main__":
   signal.signal(signal.SIGINT, signal_handler)
+
+  print ("input 1 to curse set/-")
+  print ("input 2 to curse resume/+")
+  print ("input 3 to curse cancel")
+  print ("input q to quit")
+
   main()
 
