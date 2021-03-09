ef718d7a8da70a414e5070702b4937eb46e874e6
Golden Li <lixin2@csvw.com>
2021-03-09 08:02:24 +0800

    fix parameter error

--- common/op_params.py
+++ common/op_params.py
@@ -98,8 +98,7 @@ class opParams:
                         'use_lqr': Param(False, bool, 'Enable this to use LQR as your lateral controller over default with any car'),
                         'corollaTSS2_use_indi': Param(False, bool, 'Enable this to use INDI for lat with your TSS2 Corolla'),
                         'rav4TSS2_use_indi': Param(False, bool, 'Enable this to use INDI for lat with your TSS2 RAV4'),
-                        'standstill_hack': Param(False, bool, 'Some cars support stop and go, you just need to enable this'),
-                        'lane_offset': Param(0.0, VT.number, 'In lane offset to use in lane_planner.py', live=True)} # golden patched
+                        'standstill_hack': Param(False, bool, 'Some cars support stop and go, you just need to enable this')}
 
     self._params_file = '/data/op_params.json'
     self._backup_file = '/data/op_params_corrupt.json'
